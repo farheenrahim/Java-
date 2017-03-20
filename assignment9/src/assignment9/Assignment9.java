bd8eaadb219470ed19f9af7ee767c63e193985ed
farheenrahim <farheen.rahim1@gmail.com>
2017-03-19 22:04:04 -0400

    nested for loop

---
+++ assignment9/src/assignment9/Assignment9.java
@@ -0,0 +1,23 @@
+
+package assignment9;
+
+public class Assignment9 {
+
+        public static void main(String[] args) {
+//            int[] table1= new int[10];
+//       int[] table2=new int[10];
+//       int[] table3=new int[10];
+//       int[] table4=new int[10];
+//       int[] table5=new int[10];
+       for(int i=1;i<6;i++)
+       {
+           System.out.println("The multiples of "+i);
+                     for(int j=1;j<11;j++)
+           {
+               System.out.println(i+" * "+j+"="+(i*j));
+           }
+           System.out.println("\n");
+       }
+        }
+}
+    
